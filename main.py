b0cc79b1ba3b843737e7226d53371719ddbc0dce
vicky1126 <61371976+vicky1126@users.noreply.github.com>
2020-03-14 11:11:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import glob
+import os
+from flask import Flask, render_template
+server = Flask("Vicky")
+
+@server.route("/")
+def index():
+    ds = glob.glob("diary/*")
+    ds = [d.split("/")[-1] for d in ds]
+    return render_template("index.html", dirs = ds)
+
+@server.route("/diary/<c>")
+def diary(c):
+    fs = glob.glob("diary/" + c +"/*.txt")
+    contents = []
+    for i, fn in enumerate(fs):
+        name = os.path.split(fn)[-1].replace(".txt","")
+        f = open(fn)
+        diary = f.read()
+        f.close()
+        contents.append((name, diary, i))
+
+    return render_template("diary.html", cs = contents)
+
+server.run(debug=True,
+           host="0.0.0.0",
+           port="3000")
